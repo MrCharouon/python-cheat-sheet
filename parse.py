4a346f21dddb2bf9f8900e342313ffecf0ab4196
MrCharouon <ali.khatami2009@gmail.com>
2022-08-01 18:11:26 +0430

    create man page with argparse in python

--- parse.py
+++ parse.py
@@ -1,1 +1,10 @@
+import argparse
 
+parser = argparse.ArgumentParser(description="The following is a help document")
+parser.add_argument('--add', action='store', dest='invest', help='add a new invest')
+args = parser.parse_args()
+invest = (args.invest)
+if (invest != None):
+    print('Input: ' + invest )
+    exit(0)
+print('Hello World!')
